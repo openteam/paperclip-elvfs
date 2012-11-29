a576d2378e7126b5415f011649c95ede49ec61b7
Dmitry Lihachev <lda@openteam.ru>
2012-11-29 14:47:53 +0700

    удалалять не файл, а папку

--- lib/paperclip/storage/elvfs.rb
+++ lib/paperclip/storage/elvfs.rb
@@ -8,7 +8,7 @@ module Paperclip
 
       def exists?(style_name = default_style)
         if original_filename
-          !!directory_json(path(style_name))['files'].select{|file| file['name'] == original_filename.gsub(/\+/, ' ')}.first
+          !!file_json(path(style_name))
         else
           false
         end
@@ -18,6 +18,7 @@ module Paperclip
         @queued_for_write.each do |style_name, file|
           curl = Curl::Easy.new(storage_url("#{File.dirname(path(:style_name))}?cmd=upload&target=#{directory_target(path(:style_name))}")) do |curl|
             curl.multipart_form_post = true
+            curl.on_success{|response| JSON.parse(response.body_str)['added'].first['url'] }
           end
           curl.http_post(Curl::PostField.file('upload[]', file.path, File.basename(path(:style_name))))
         end
@@ -28,7 +29,10 @@ module Paperclip
       end
 
       def flush_deletes #:nodoc:
-        # TODO: Please! Implement this method.
+        @queued_for_delete.each do |path|
+          log("deleting #{path}")
+          delete_file(path)
+        end
         @queued_for_delete = []
       end
 
@@ -37,8 +41,19 @@ module Paperclip
           JSON.parse(open(storage_url("#{File.dirname(path)}?cmd=open&init=true")).read)
         end
 
+        def file_json(path)
+          directory_json(path)['files'].select{|file| file['name'] == File.basename(path).gsub(/\+/, ' ')}.first
+        end
+
+
         def directory_target(path)
           directory_json(path)['cwd']['hash']
+        end
+
+        def delete_file(path)
+          if file_hash = file_json(File.dirname(path))
+            open(storage_url("#{File.dirname(File.dirname(path))}?cmd=rm&targets[]=#{file_hash['hash']}"))
+          end
         end
 
         def storage_url(path='')
