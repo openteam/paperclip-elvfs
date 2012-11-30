5c55413d07d5d303206d61176b6cb418b4655db9
Dmitry Lihachev <lda@openteam.ru>
2012-11-30 09:41:38 +0700

    include application name into path to file

--- lib/paperclip/elvfs/settings.rb
+++ lib/paperclip/elvfs/settings.rb
@@ -1,8 +1,12 @@
 Paperclip::Schema::COLUMNS.merge!(:url => :text)
 
 Paperclip::Attachment.default_options[:storage] = :elvfs
-Paperclip::Attachment.default_options[:path]    = ':attachment/:date/:id/:filename'
+Paperclip::Attachment.default_options[:path]    = ':app_name/:attachment/:date/:id/:filename'
 #Paperclip::Attachment.default_options[:url]     = :file_url
+
+Paperclip.interpolates :app_name do |attachment, style|
+  Rails.application.railtie_name.sub /_application$/, ''
+end
 
 Paperclip.interpolates :date do |attachment, style|
   Date.today.strftime '%Y/%m/%d'
