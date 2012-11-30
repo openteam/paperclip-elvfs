ba6c9c0c281d70d1045906d46d6e456905dae7d4
Dmitry Lihachev <lda@openteam.ru>
2012-11-30 11:21:33 +0700

    добавлено имя модели в путь е файлу, model.file.url ведёт себя так как надо

--- lib/paperclip/elvfs/settings.rb
+++ lib/paperclip/elvfs/settings.rb
@@ -2,8 +2,20 @@
 Paperclip::Schema::COLUMNS.merge!(:url => :text)
 
 # default settings
-Paperclip::Attachment.default_options[:path]      = ':app_name/:attachment/:date/:id/:filename'
-Paperclip::Attachment.default_options[:url]       = :file_url
+Paperclip::Attachment.default_options[:path]       = ':app_name/:class/:attachment/:date/:id/:filename'
+Paperclip::Attachment.default_options[:url]        = :file_url
+Paperclip::Attachment.default_options[:escape_url] = false
+
+# TODO: remove this warn when paperclip 3.3.2 will be released
+class Paperclip::Attachment
+  def url_with_unescape(style_name = default_style, options = {})
+    ActiveSupport::Deprecation.warn "Remove this warning when paperclip 3.3.2 will be released"
+    options[:escape] = false
+    url_without_unescape(style_name, options)
+  end
+
+  alias_method_chain :url, :unescape
+end
 
 # our interpolators
 Paperclip.interpolates :app_name do |attachment, style|
